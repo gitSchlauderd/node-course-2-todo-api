30c9de9a471241b2a50e0c2d1e919a179b2479fd
Jeff <jcschlauder@gmail.com>
2018-01-05 15:52:35 -0700

    Add POST /todos route and refactor Mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+//use these 2 lines to connect to server + set up mongoose promises
+
+module.exports = (mongoose);
---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+}); //this is how to create a mongoose model to be used
+
+module.exports = {Todo};
