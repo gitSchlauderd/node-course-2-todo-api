27f6893b83cfb90019616396993a56aef45b2f0f
Jeff <jcschlauder@gmail.com>
2018-01-06 12:31:44 -0700

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,4 +4,4 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 //use these 2 lines to connect to server + set up mongoose promises
 
-module.exports = (mongoose);
+module.exports = {mongoose};
